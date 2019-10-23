f85c4f645e29708fe7d43f6d8fb0bddf4f0e2e69
aidan tsang <aidan.tsang20@ycdsbk12.ca>
2019-10-23 10:41:31 -0400

    a

--- amazonfc.py
+++ amazonfc.py
@@ -1,7 +1,8 @@
 from typing import *
 
-categories = ("automotive", "clothings", "electronics", "home and kitchen", "industrial", "sports", "tools", "toys and games")
+prod_categories = ("automotive", "clothings", "electronics", "home and kitchen", "industrial", "sports", "tools", "toys and games")
 shipping_box_size = ("12x12x12in", "16x16x16in", "18x18x18in", "22x22x22in", "24x24x24in", "oversize")
+distance = ("short_distance", "mid_range", "long_distance", "international")
 
 class Shelf:
     def __init__(self, num):
@@ -22,18 +23,22 @@ class Compartment:
     def __str__(self):
         return f"content: {self.content}"
 
-    def add():
-        
+    def add(self, prod):
+        self.content.append(prod)
+
+    def remove(self, prod):
+        self.content.remove(prod)    
 
 
 class Product:
-    def __init__(self, name: str, image: str, category: int):
+    def __init__(self, name: str, image: str, category: int, code: int):
         self.name = name
         self.image = image
-        self.cat = categories[category]
+        self.cat = prod_categories[category]
+        self.code = code
     
     def __str__(self):
-        return f"{self.name}, {self.cat}"
+        return f"{self.name}, {self.cat}, {self.code}"
 
     def packaging(self, explosive: bool, fragile: bool, flamable: bool, size: int):
         self.explosive = explosive
@@ -42,15 +47,40 @@ class Product:
         self.box_size = shipping_box_size[size]
         self.packaged = True
 
+
+class Cart:
+    def __init__(self):
+        self.content = []
+
+    def add(self, item):
+        self.content.append(item)
+
+    def remove(self, item):
+        self.content.remove(item)
+
+
+class Truck:
+    def __init__(self, types):
+        self.type = distance[types]
+        self.content = []
+
+    def loading(self, prod):
+        self.content.append(prod)
+
+    def leave(self):
+        del self
+    
+
 #Ship In
 def scan_prod_to_trolly():
-    """Create Product"""
+    """Create Product and move to trolly"""
     pass
 
 
 def scan_prod_to_shelf():
-    """Put Product in shelf/compartment"""
+    """Put Product from trolly to shelf/compartment"""
     pass
+
 
 #Ship Out
 def display_box_type():
@@ -62,22 +92,27 @@ def stamp_code():
     """Stamp barcode and address"""
     pass
 
+
 def send_to_truck():
     """Product send to truck"""
     pass
+
 
 #Order Fulfillment
 def display_prod():
     """Display product"""
     pass
 
+
 def get_prod_from_shelf():
     """Scan product out of shelf"""
     pass
 
+
 def put_prod_in_bin():
     """Place product into bin"""
     pass
+
 
 def package_bin():
     """Send bin to packaging"""
@@ -85,3 +120,5 @@ def package_bin():
 
 
 storage = [Shelf(i+1) for i in range(8)]
+trolly = Cart()
+bins = Cart()
