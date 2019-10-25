243d1c3a5f4357b9cec520e4f22efba1e73bce11
aidan tsang <aidan.tsang20@ycdsbk12.ca>
2019-10-25 11:04:56 -0400

    a

--- GUI.py
+++ GUI.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
 from PyQt5.QtGui import QIcon
+from amazonfc import *
+
 
 class App(QWidget):
 
@@ -12,36 +14,40 @@ class App(QWidget):
         self.width = 640
         self.height = 480
         self.initUI()
-    
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        
+
         self.getInteger()
         self.getText()
         self.getDouble()
         self.getChoice()
-        
+
         self.show()
-        
+
     def getInteger(self):
-        i, okPressed = QInputDialog.getInt(self, "Get integer","Percentage:", 28, 0, 100, 1)
+        i, okPressed = QInputDialog.getInt(self, "Get integer", "Percentage:",
+                                           28, 0, 100, 1)
         if okPressed:
             print(i)
 
     def getDouble(self):
-        d, okPressed = QInputDialog.getDouble(self, "Get double","Value:", 10.50, 0, 100, 10)
+        d, okPressed = QInputDialog.getDouble(self, "Get double", "Value:",
+                                              10.50, 0, 100, 10)
         if okPressed:
-            print( d)
-        
+            print(d)
+
     def getChoice(self):
-        items = ("Red","Blue","Green")
-        item, okPressed = QInputDialog.getItem(self, "Get item","Color:", items, 0, False)
+        items = ("Red", "Blue", "Green")
+        item, okPressed = QInputDialog.getItem(self, "Get item", "Color:",
+                                               items, 0, False)
         if okPressed and item:
             print(item)
 
     def getText(self):
-        text, okPressed = QInputDialog.getText(self, "Get text","Your name:", QLineEdit.Normal, "")
+        text, okPressed = QInputDialog.getText(self, "Get text", "Your name:",
+                                               QLineEdit.Normal, "")
         if okPressed and text != '':
             print(text)
 
@@ -50,4 +56,3 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
-
