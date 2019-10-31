c49864020f1d4992e42997a71861428d269da507
Al3xanderLee <34520973+Al3xanderLee@users.noreply.github.com>
2019-10-31 11:11:25 -0400

    Update test_amazonfc.py

--- test_amazonfc.py
+++ test_amazonfc.py
@@ -1,26 +1,24 @@
 from amazonfc import *
 
-
 def test_can_create_shelf():
     pass
-
 
 def test_can_create_product():
     product = Product("Baby powder", "babypowder.png", "Health & Personal Care", 123456)
     assert product.name == "Baby powder"
     assert product.image == "babypowder.png"
-    assert product.cat == "Health & Personal Care"
+    assert product.cate == "Health & Personal Care"
     assert product.code == 123456
 
 
 def test_can_create_package():
     product = Product("Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567)
     product.package(True, False, True, "Small")
-    assert product.explosive is True
-    assert product.fragile is False
-    assert product.flammable is True
+    assert product.explosive == True
+    assert product.fragile == False
+    assert product.flammable == True
     assert product.box_size == "Small"
-    assert product.packaged is True
+    assert product.packaged == True
 
 
 def test_can_create_stamp_code():
@@ -30,7 +28,6 @@ def test_can_create_stamp_code():
     stamp_code = product.stamp_code("ABC-abc-1234")
     assert product.barcode == "ABC-abc-1234"
     assert product.address == "somewhere"
-
 
 def test_can_use_cart():
     product = Product("Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567)
@@ -42,65 +39,41 @@ def test_can_use_cart():
 
 
 def test_can_create_bin():
-    pass
+    bin_1 = Bin()
+    assert bin_1.content == []
+    assert bin_1.id == 0
+  
+    assert bin_1.add() == 
 
+def test_compartment():
+        pass
 
-def test_package():
-    pass
-
-
-def test_send_to_truck():
-    pass
-
-
-def test_can_create_truck():
-    pass
-
-
-def test_loading():
+def test_truck():
     product = Product("Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567)
     truck = Truck(1)
     truck.loading(product)
     assert truck.content[0] == product
 
-
-def test_leave():
     truck = Truck(1)
     truck.leave
     assert len(truck.content) == 0
 
 
-def test_can_create_request():
+def test_request():
     request = Request("123 pitbull street", "International", "chromebook", 123)
+    assert request.address == "123 pitbull street"
+    assert request.dis == "International"
+    assert request.prod_name == "chromebook"
+    assert request.prod_code == 123
 
+    request = Request("1234 sesame street", "Local", "table", 444)
+    assert request.address == "1234 sesame street"
+    assert request.dis == "Local"
+    assert request.prod_name == "table"
+    assert request.prod_code == 444
 
-def test_get_prod_id():
-    pass
-
-
-def test_link_prod():
-    pass
-
-
-def test_scan_prod_to_trolly():
-    pass
-
-
-def test_display_box_type():
-    pass
-
-
-def test_order_fulfillment():
-    pass
-
-
-def test_display_prod():
-    pass
-
-
-def test_get_prod_request():
-    pass
-
-
-def test_get_prod_from_shelf():
-    pass
+    request = Request("12345 king street", "International", "Book", 555)
+    assert request.address == "12345 king street"
+    assert request.dis == "International"
+    assert request.prod_name == "Book"
+    assert request.prod_code == 555
