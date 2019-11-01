f3ac503fc1f7d3c3962df8afe6964c6155e47ba2
aidan tsang <aidan.tsang20@ycdsbk12.ca>
2019-11-01 10:00:39 -0400

    a

--- test_amazonfc.py
+++ test_amazonfc.py
@@ -2,25 +2,44 @@ from amazonfc import *
 
 
 def test_can_create_shelf():
-    pass
+    shelf = Shelf(1)
+    assert shelf.num == 1
+
+    shelf = Shelf(2)
+    assert shelf.num == 2
+
+    shelf = shelf(35)
+    assert shelf.num == 35
 
 
 def test_can_create_product():
     product = Product("Baby powder", "babypowder.png", "Health & Personal Care", 123456)
     assert product.name == "Baby powder"
     assert product.image == "babypowder.png"
-    assert product.cat == "Health & Personal Care"
+    assert product.cate == "Health & Personal Care"
     assert product.code == 123456
+
+    product = Product("chromebook", "chromebook.png", "Electronics", 123457)
+    assert product.name == "chromebook"
+    assert product.image == "chromebook.png"
+    assert product.cate == "Electronics"
+    assert product.code == 123457
+
+    product = Product("Lotion", "lotion.png", "Health & Personal Care", 123556)
+    assert product.name == "Lotion"
+    assert product.image == "lotion.png"
+    assert product.cate == "Health & Personal Care"
+    assert product.code == 123556
 
 
 def test_can_create_package():
     product = Product("Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567)
     product.package(True, False, True, "Small")
-    assert product.explosive is True
-    assert product.fragile is False
-    assert product.flammable is True
+    assert product.explosive == True
+    assert product.fragile == False
+    assert product.flammable == True
     assert product.box_size == "Small"
-    assert product.packaged is True
+    assert product.packaged == True
 
 
 def test_can_create_stamp_code():
@@ -42,65 +61,60 @@ def test_can_use_cart():
 
 
 def test_can_create_bin():
-    pass
-
-
-def test_package():
-    pass
-
-
-def test_send_to_truck():
-    pass
-
-
-def test_can_create_truck():
-    pass
-
-
-def test_loading():
+    bin_1 = Bin()
+    assert bin_1.content == []
+    assert bin_1.id == 0
+  
     product = Product("Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567)
+    bin_1.add(product)
+    assert bin_1.content == product
+
+    bin_1.remove(product)
+    assert bin_1.remove == []
+
+
+def test_compartment():
+    compartment = Compartment()
+    assert compartment.content == []
+    prod = Product("Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567)
+
+    compartment.add(prod)
+    assert content == ["Hair spray", "hair_spray.png", "Beauty & Personal Care", 234567]
+
+    compartment.remove(prod)
+    assert content == []
+
+
+def test_truck():
     truck = Truck(1)
+    assert truck.content == []
+    assert truck.type == 1
+    assert truck.id == 1
+    assert truck.all_trucks == [1]
+
+    product = Request("1234 sesame street", "1", "table", 444)
     truck.loading(product)
     assert truck.content[0] == product
 
-
-def test_leave():
-    truck = Truck(1)
     truck.leave
     assert len(truck.content) == 0
 
 
-def test_can_create_request():
+def test_request():
     request = Request("123 pitbull street", "International", "chromebook", 123)
+    assert request.address == "123 pitbull street"
+    assert request.dis == "International"
+    assert request.prod_name == "chromebook"
+    assert request.prod_code == 123
 
+    request = Request("1234 sesame street", "Local", "table", 444)
+    assert request.address == "1234 sesame street"
+    assert request.dis == "Local"
+    assert request.prod_name == "table"
+    assert request.prod_code == 444
 
-def test_get_prod_id():
-    pass
-
-
-def test_link_prod():
-    pass
-
-
-def test_scan_prod_to_trolly():
-    pass
-
-
-def test_display_box_type():
-    pass
-
-
-def test_order_fulfillment():
-    pass
-
-
-def test_display_prod():
-    pass
-
-
-def test_get_prod_request():
-    pass
-
-
-def test_get_prod_from_shelf():
-    pass
+    request = Request("12345 king street", "International", "Book", 555)
+    assert request.address == "12345 king street"
+    assert request.dis == "International"
+    assert request.prod_name == "Book"
+    assert request.prod_code == 555
